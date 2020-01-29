42405a08043dea51998d0677ffd1fec072d2c0cc
carlesra <juan.ramos.a@ies1xabia.org>
2020-01-29 18:01:42 +0100

    cambie el nom dels atributs del model

--- Model/Equipo.cs
+++ Model/Equipo.cs
@@ -8,5 +8,37 @@ namespace Model
 {
     class Equipo
     {
+        private int idEquipo;
+        private string nombre;
+
+        public Equipo(int idEquipo, string nombre)
+        {
+            this.idEquipo = idEquipo;
+            this.nombre = nombre;
+        }
+
+        public int IdEquipo
+        {
+            get
+            {
+                return idEquipo;
+            }
+            set
+            {
+                idEquipo = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
     }
 }
--- Model/Jugador.cs
+++ Model/Jugador.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Class1
+    public enum Posiciones
     {
+        ESCOLTA, PIVOT, ALERO, ALA_PIVOT, BASE
+    }
+
+    public class Jugador
+    {
+        private readonly int ID;
+        private string NOMBRE;
+        private string APELLIDO;
+        private Posiciones POSICION;
+        private DateTime FECHA_ALTA;
+        private float SALARIO;
+        private int EQUIPO;
+        private float ALTURA;
+
+        public Jugador(int id, string nombre, string apellido, Posiciones posicion,
+            DateTime fechaAlta, float salario, int idEquipo, float altura)
+        {
+            this.ID = id;
+            this.NOMBRE = nombre;
+            this.APELLIDO = apellido;
+            this.POSICION = posicion;
+            this.FECHA_ALTA = fechaAlta;
+            this.SALARIO = salario;
+            this.EQUIPO = idEquipo;
+            this.ALTURA = altura;
+        }
+
+        public string Nombre
+        {
+            get { return NOMBRE;  }
+            set { NOMBRE = value; }
+        }
+
+        public string Apellido
+        {
+            get { return APELLIDO; }
+            set { APELLIDO = value; }
+        }
+
+        public Posiciones Posicion
+        {
+            get { return POSICION; }
+            set { POSICION = value; }
+        }
+
+        public DateTime FechaAlta
+        {
+            get { return FECHA_ALTA; }
+            set { FECHA_ALTA = value; }
+        }
+
+        public float Salario
+        {
+            get { return SALARIO; }
+            set { SALARIO = value; }
+        }
+
+        public int IdEquipo
+        {
+            get { return EQUIPO; }
+            set { EQUIPO = value; }
+        }
+
+        public float Altura
+        {
+            get { return ALTURA; }
+            set { ALTURA = value; }
+        }
     }
 }
